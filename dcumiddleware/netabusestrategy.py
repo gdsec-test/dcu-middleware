f0f83880dd013713a23400e19e4d77063b332c3d
lhalvorson <lhalvorson@godaddy.com>
2016-04-14 13:46:17 -0700

    Added MalwareStrategy and netabusestrategy as well as unit tests.

--- dcumiddleware/netabusestrategy.py
+++ dcumiddleware/netabusestrategy.py
@@ -1,12 +1,42 @@
 import logging
 
 from dcumiddleware.interfaces.strategy import Strategy
+from dcumiddleware.phishstorymongo import PhishstoryMongo
+from dcumiddleware.urihelper import URIHelper
 
 
 class NetAbuseStrategy(Strategy):
 
-    def __init__(self):
+    def __init__(self, settings):
         self._logger = logging.getLogger(__name__)
+        self._urihelper = URIHelper(settings)
+        self._db = PhishstoryMongo(settings)
 
     def process(self, data, **kwargs):
         super(NetAbuseStrategy, self).process(data)
+
+        # determine if IP is hosted with godaddy
+        hosted_status = self._urihelper.get_status(data.domain)
+        if hosted_status == URIHelper.HOSTED:
+            status = "HOSTED"
+        elif hosted_status == URIHelper.REG:
+            status = "REGISTERED"
+        elif hosted_status == URIHelper.NOT_REG_HOSTED:
+            status = "FOREIGN"
+        else:
+            self._logger.warn("Unknown hosted status for incident: {}".format(data))
+            status = "UNKNOWN"
+
+        # Add hosted_status attribute to incident
+        data.hosted_status = status
+        # save the incident to the database
+        if status is "HOSTED":
+            iid = self._db.add_new_incident(data.ticketId, data.as_dict())
+            if iid:
+                self._logger.info("Incident {} inserted into database".format(iid))
+            else:
+                self._logger.error("Unable to insert {} into database".format(iid))
+        else:
+            data.close_reason = "NETABUSE: Not Hosted"
+            self._db.close_incident(data.ticketId, data.as_dict())
+
--- tests/phishingstrategy_tests.py
+++ tests/phishingstrategy_tests.py
@@ -31,5 +31,5 @@ class TestPhishingStrategy:
                         'sources': u'http://comicsn.beer/uncategorized/casual-gaming-and-the-holidays/',
                         'type': u'PHISHING'}
         self._phishing.process(Incident(test_record))
-        lst =  [doc for doc in self._phishing._db.get_open_tickets(PhishstoryDB.PHISHING)]
+        lst = [doc for doc in self._phishing._db.get_open_tickets(PhishstoryDB.PHISHING)]
         assert_true(lst[0]['ticketId'] == 'DCU000001053')
