aa54b8e74bdd785fd16530986cb0afa19fbbb09e
ebenson <ebenson@godaddy.com>
2016-05-25 10:42:44 -0700

    updated ote db path and settings tests

--- tests/settings_tests.py
+++ tests/settings_tests.py
@@ -10,11 +10,11 @@ class TestSettings:
 		os.environ['DBPASS'] = 'vkdE4NSw5wgFIcQDxQ=='  # decrypted password: test_password
 		path = os.getcwd()+'/tests/'
 		os.environ['KEYFILE'] = path+'test_key.txt'
-		os.environ['AUTHPASS'] = 'gDUP2rTdDPk='  # decrypted password: JmpxL72s
+		os.environ['AUTHPASS'] = 'vkdE4NSw5wgFIcQDxQ=='  # decrypted password: test_password
 
 	def test_app_config(self):
 		configs = settings.AppConfig()
-		assert_true(configs.AUTHPASS == 'JmpxL72s')
+		assert_true(configs.AUTHPASS == 'test_password')
 
 	def test_production_app_config(self):
 		configs = settings.ProductionAppConfig()
@@ -24,4 +24,4 @@ class TestSettings:
 	def test_ote_app_config(self):
 		configs = settings.OTEAppConfig()
 		assert_true(configs.PASS == 'vkdE4NSw5wgFIcQDxQ==')
-		assert_true(configs.DBURL == 'mongodb://sau_p_phish:test_password@10.22.9.209/phishstory')
+		assert_true(configs.DBURL == 'mongodb://sau_o_phish:test_password@10.22.9.209/otephishstory')
