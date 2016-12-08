ab355aa6c28cf66f23dcf1ef1e333b3f82a5535b
Chris Hauser <chauser@godaddy.com>
2016-12-08 14:19:00 -0700

    Fixed issue with parsing network name

--- dcumiddleware/urihelper.py
+++ dcumiddleware/urihelper.py
@@ -135,7 +135,9 @@ class URIHelper:
             return server_domain == "secureserver"
         except Exception as e:
             self._logger.warning("Error in reverse DNS lookup %s : %s, attempting whois lookup..", ip, e.message)
-            return 'GODADDY' in IPWhois(ip).lookup_rdap().get('network',[]).get('name', '').replace('-', '')
+            regex = re.compile('[^a-zA-Z]')
+            name = regex.sub('', IPWhois(ip).lookup_rdap().get('network',[]).get('name', ''))
+            return 'GODADDY' in name.upper()
 
     def domain_whois(self, domain_name):
         """
