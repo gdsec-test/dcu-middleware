c422f7de16a406d6eb88b5ea17a78b98bb59d561
Chris Hauser <chauser@godaddy.com>
2017-04-11 19:22:09 -0700

    optimize Dockerfile and cleanup setup

--- Dockerfile
+++ Dockerfile
@@ -23,21 +23,16 @@ curl -L https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x8
 ln -s /usr/local/share/phantomjs-2.1.1-linux-x86_64/bin/phantomjs /usr/bin/phantomjs
 
 # Make directory for middleware
-RUN mkdir -p /app
-
-WORKDIR /app
-
-# Move files to new dir
-ADD . /app
-RUN chown -R dcu:dcu /app
+COPY ./run.py ./settings.py ./logging.yml ./celeryconfig.py ./health.sh /app/
+COPY . /tmp/
 
 # pip install private pips staged by Makefile
 RUN for entry in dcdatabase blindAl; \
     do \
-    pip install --compile "/app/private_pips/$entry"; \
+    pip install --compile "/tmp/private_pips/$entry"; \
     done
 
-RUN pip install --compile -r requirements.txt
+RUN pip install --compile /tmp
 
 # cleanup
 RUN apt-get remove --purge -y build-essential \
@@ -46,8 +41,11 @@ RUN apt-get remove --purge -y build-essential \
     libssl-dev \
     python-dev && \
     rm -rf /var/lib/apt/lists/* && \
-    rm -rf /app/private_pips
+    rm -rf /tmp && \
+    chown -R dcu:dcu /app
+
 
 USER dcu
+WORKDIR /app
 
 CMD ["/usr/local/bin/celery", "-A", "run", "worker", "-l", "INFO"]
--- setup.py
+++ setup.py
@@ -1,11 +1,9 @@
-#!/usr/bin/env python
-from distutils.core import setup
-
+from setuptools import setup, find_packages
 from pip.req import parse_requirements
 
 # parse_requirements() returns generator of pip.req.InstallRequirement objects
-install_reqs = parse_requirements('requirements.txt')
-testing_reqs = parse_requirements('test_requirements.txt')
+install_reqs = parse_requirements('requirements.txt', session=False)
+testing_reqs = parse_requirements('test_requirements.txt', session=False)
 
 # reqs is a list of requirement
 # e.g. ['django==1.5.1', 'mezzanine==1.4.6']
@@ -13,17 +11,15 @@ reqs = [str(ir.req) for ir in install_reqs]
 test_reqs = [str(ir.req) for ir in testing_reqs]
 
 
-setup(name='dcumiddleware',
-      version='1.0',
-      description='middleware for processing API requests',
-      author='DCU',
-      author_email='dcueng@godaddy.com',
-      url='https://github.secureserver.net/ITSecurity/DCUMiddleware',
-      # packages=packages,
-      package_dir={'dcumiddleware': 'dcumiddleware'},
-      packages = ['dcumiddleware', 'dcumiddleware.interfaces'],
-      include_package_data=True,
-      install_requires=reqs,
-      tests_require=test_reqs,
-      test_suite="nose.collector"
-     )
+setup(
+    name='dcumiddleware',
+    version='1.0',
+    description='middleware for processing API requests',
+    author='DCU',
+    author_email='dcueng@godaddy.com',
+    url='https://github.secureserver.net/ITSecurity/DCUMiddleware',
+    # packages=packages,
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    install_requires=reqs,
+    tests_require=test_reqs,
+    test_suite="nose.collector")
