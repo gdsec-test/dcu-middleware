15e505d249f2037d867ed328cba78835cac1d6aa
Chris Hauser <chauser@godaddy.com>
2017-04-28 12:37:10 -0700

    fixed indentation

--- dcumiddleware/cmapservicehelper.py
+++ dcumiddleware/cmapservicehelper.py
@@ -14,28 +14,27 @@ class CmapServiceHelper(object):
 
     def cmap_query(self, query, domain):
         """
-    Returns query result of cmap service given a query
-    :param query:
-    :param domain:
-    :return query result:
-    """
+        Returns query result of cmap service given a query
+        :param query:
+        :param domain:
+        :return query result:
+        """
         try:
             with sessions.Session() as session:
                 self._logger.info("Fetching query for {}".format(domain))
                 re = session.request(method='POST', url=self._graphene_url, headers=self._post_headers, data=query)
                 return json.loads(re.text)
         except Exception as e:
-            self._logger.error("Unable to query CMAP service for: {}. {}".
-                               format(domain, e.message))
+            self._logger.error("Unable to query CMAP service for: {}. {}".format(domain, e.message))
             return None
 
     def domain_query(self, domain):
         """
-    Returns query result of cmap service given a domain
-      :param domain:
-      :return query result: query result host, registrar, domain create date, vip profile, shopperID, shopper create date,
-      shopper domain count, API parent/child account numbers
-    """
+        Returns query result of cmap service given a domain
+        :param domain:
+        :return query result: query result host, registrar, domain create date, vip profile, shopperID, shopper create date,
+        shopper domain count, API parent/child account numbers
+        """
         try:
             query = ('''
           {
@@ -65,10 +64,8 @@ class CmapServiceHelper(object):
           }
           ''')
             query_result = self.cmap_query(query, domain)
-            query_result['data']['domainQuery']['registrar']['createDate'] = \
-             self._date_time_format(query_result['data']['domainQuery']['registrar']['createDate'])
-            query_result['data']['domainQuery']['shopperInfo']['dateCreated'] = \
-             self._date_time_format(query_result['data']['domainQuery']['shopperInfo']['dateCreated'])
+            query_result['data']['domainQuery']['registrar']['createDate'] = self._date_time_format(query_result['data']['domainQuery']['registrar']['createDate'])
+            query_result['data']['domainQuery']['shopperInfo']['dateCreated'] = self._date_time_format(query_result['data']['domainQuery']['shopperInfo']['dateCreated'])
             return query_result
         except Exception as e:
             self._logger.error("Unable to complete domain query for : {}. {}".
@@ -77,10 +74,10 @@ class CmapServiceHelper(object):
 
     def _date_time_format(self, date):
         """
-    Returns date/time formated object
-    :param date:
-    :return iso_date:
-    """
+        Returns date/time formated object
+        :param date:
+        :return iso_date:
+        """
         try:
             iso_date = datetime.strptime(date, '%Y-%m-%d')
             return iso_date
@@ -92,11 +89,11 @@ class CmapServiceHelper(object):
 
     def api_cmap_merge(self, apidata, cmapdata):
         """
-    Returns query result of cmap service given a domain
-    :param apidata:
-    :param cmapdata:
-    :return merged_data: dictionary that is the result of merging the api data and cmap data dictionaries
-    """
+        Returns query result of cmap service given a domain
+        :param apidata:
+        :param cmapdata:
+        :return merged_data: dictionary that is the result of merging the api data and cmap data dictionaries
+        """
         try:
             merged_data = dict(apidata.items() + cmapdata.items())
             return merged_data
