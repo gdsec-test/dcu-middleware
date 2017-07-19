a657977ca9a910fd1ae3b5ce3ff9b40f0df360f4
nlemay <nlemay@godaddy.com>
2017-07-19 15:32:28 -0700

    Refactored Malware to match Phishing changes and removed duplication

--- dcumiddleware/interfaces/strategy.py
+++ dcumiddleware/interfaces/strategy.py
@@ -1,4 +1,5 @@
 import abc
+import re
 
 
 class Strategy(object):
@@ -15,3 +16,38 @@ class Strategy(object):
         :param data:
         :return:
         """
+
+    def parse_hostname_and_registrar(self, data):
+        """
+        Returns the host and registrar as a tuple from the data provided.
+        :param data:
+        :return:
+        """
+        # regex to determine if godaddy is the host and registrar
+        regex = re.compile('[^a-zA-Z]')
+
+        host = data.get('data', {}).get('domainQuery', {}).get('host', {}).get('name', None)
+        hostname = regex.sub('', host) if host is not None else None
+        reg = data.get('data', {}).get('domainQuery', {}).get('registrar', {}).get('name', None)
+        registrar = regex.sub('', reg) if reg is not None else None
+
+        return hostname, registrar
+
+    def is_blacklisted(self, data):
+        """
+        Returns a Boolean that represents whether or not a shopper or a domain is blacklisted.
+        :param data:
+        :return:
+        """
+        shopper_blacklist = data.get('data', {}).get('domainQuery', {}).get('shopperInfo', {}).get('vip', {}).get('blacklist', True)
+        domain_blacklist = data.get('data', {}).get('domainQuery', {}).get('blacklist', True)
+
+        return shopper_blacklist is True or domain_blacklist is True
+
+    def is_unconfirmed_vip(self, data):
+        """
+        Returns a Boolean representing whether or not a shopper has an unconfirmed VIP status or not.
+        :param data:
+        :return:
+        """
+        return data.get('data', {}).get('domainQuery', {}).get('shopperInfo', {}).get('shopperId', None) is None
