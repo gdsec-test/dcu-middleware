98645e1b7e8412b53670650ef5301aeb12696496
nlemay <nlemay@godaddy.com>
2017-07-20 15:15:09 -0700

    Updated netabuse to more closely match other Strategys

--- dcumiddleware/interfaces/strategy.py
+++ dcumiddleware/interfaces/strategy.py
@@ -8,6 +8,23 @@ class Strategy(object):
     """
     __metaclass__ = abc.ABCMeta
 
+    UNRESOLVABLE = "unresolvable"
+    UNWORKABLE = "unworkable"
+
+    HOSTED = "HOSTED"
+    REGISTERED = "REGISTERED"
+    FOREIGN = "FOREIGN"
+    UNKNOWN = "UNKNOWN"
+
+    @abc.abstractmethod
+    def close_process(self, data, close_reason):
+        """
+        Close a particular ticket given a close_reason
+        :param data:
+        :param close_reason:
+        :return:
+        """
+
     @abc.abstractmethod
     def process(self, data, **kwargs):
         """
