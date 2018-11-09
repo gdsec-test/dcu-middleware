c66bf60a630fbbd20d415768ac2531eb8cd51dca
kmurthy <kmurthy@godaddy.com>
2018-11-09 11:04:32 -0700

    Cmapt 2411 route 123 reg (#77)
    * Updated middleware to route 123REG tickets to GD
    
    * Updated middleware to route 123REG tickets to GD
    
    * Updated middleware to route 123REG tickets to GD
    
    * Create a function to optimize route
    
    * Create a function to optimize route
    
    * Remove extra blank line
    
    * Remove extra blank line
    
    * Updated routing code to incorporate additional EMEA brands;
    
    * Changed docstring and moved brands subset to the class from settings
    
    * Removed blank line

--- dcumiddleware/routinghelper.py
+++ dcumiddleware/routinghelper.py
@@ -7,7 +7,10 @@ class RoutingHelper:
     """
     _brands = {'GODADDY': 'run.process_gd',
                'FOREIGN': 'run.process_gd',
-               'EMEA': 'run.process_emea'}
+               'EMEA': 'run.process_emea',
+               '123REG': 'run.process_gd'}
+
+    _brands_routed_to_gd = {'GODADDY', '123REG', 'FOREIGN'}
 
     def __init__(self, capp, db):
         self._logger = logging.getLogger(__name__)
@@ -25,12 +28,10 @@ class RoutingHelper:
 
         brands = self._find_brands_to_route(host_brand, registrar_brand)
 
+        # Closing tickets that are EMEA only.
         if len(brands) == 1 and 'EMEA' in brands:
             # Need to be sure to return the updated data structure to Celery and EMEABS Container
             data = self._close_emea_only_ticket(data.get('ticketId'))
-        elif 'FOREIGN' in brands and 'GODADDY' in brands:
-            # All foreign tickets get sent to GoDaddy so this prevents two tickets being sent to GDBS Container
-            brands = ['GODADDY']
 
         for brand in brands:
             self._route_to_brand(brand, data)
@@ -39,25 +40,30 @@ class RoutingHelper:
 
     def _find_brands_to_route(self, host_brand, registrar_brand):
         """
-        Returns a list of either length 1 or 2, of all the brands that need to process this ticket.
+        Returns a set of either length 1 or 2, of all the brands that need to process this ticket.
+        All combinations of brands that are a subset of {'GODADDY', '123REG', 'FOREIGN'} will be routed to GoDaddy.
+        This prevents two tickets from being sent to the GDBS Container.
+        For ex.
+            1. GODADDY and 123REG will be routed to GODADDY.
+            2. 123REG and FOREIGN will be routed to GODADDY.
+            3. GODADDY and FOREIGN will be routed to GODADDY.
+
         :param host_brand:
         :param registrar_brand:
-        :return:
+        :return brands:
         """
-        brands = []
-
+        brands = set()
         if host_brand is None and registrar_brand is None:  # Anything we don't have data for go to GoDaddy
-            brands.append('GODADDY')
+            brands.add('GODADDY')
         else:
-            hosted_by_brand = host_brand in self._brands
+            if host_brand in self._brands:
+                brands.add(host_brand)
+            if registrar_brand in self._brands:
+                brands.add(registrar_brand)
 
-            if hosted_by_brand and host_brand == registrar_brand:  # Don't route two tickets for one workflow
-                brands = [host_brand]
-            else:
-                if hosted_by_brand:
-                    brands.append(host_brand)
-                if registrar_brand in self._brands:
-                    brands.append(registrar_brand)
+        if len(brands) > 1 and brands.issubset(self._brands_routed_to_gd):
+            brands = {'GODADDY'}
+
         return brands
 
     def _route_to_brand(self, service, data):
--- tests/routinghelper_tests.py
+++ tests/routinghelper_tests.py
@@ -2,7 +2,7 @@ import mongomock
 from celery import Celery
 from dcdatabase.phishstorymongo import PhishstoryMongo
 from mock import patch
-from nose.tools import assert_equal, assert_true
+from nose.tools import assert_equal
 
 from celeryconfig import CeleryConfig
 from dcumiddleware.routinghelper import RoutingHelper
@@ -19,39 +19,63 @@ class TestRoutingHelper:
 
     def test_find_brands_to_route_no_host_no_registrar(self):
         brands = self._routing_helper._find_brands_to_route(None, None)
-        assert_true(brands == ['GODADDY'])
+        assert_equal(brands, {'GODADDY'})
 
     def test_find_brands_to_route_host_no_registrar_branded(self):
         brands = self._routing_helper._find_brands_to_route('GODADDY', None)
-        assert_true(brands == ['GODADDY'])
+        assert_equal(brands, {'GODADDY'})
 
     def test_find_brands_to_route_registrar_no_host_branded(self):
         brands = self._routing_helper._find_brands_to_route(None, 'EMEA')
-        assert_true(brands == ['EMEA'])
+        assert_equal(brands, {'EMEA'})
 
     def test_find_brands_to_route_registrar_no_host_not_branded(self):
         brands = self._routing_helper._find_brands_to_route(None, 'FOREIGN')
-        assert_true(brands == ['FOREIGN'])
+        assert_equal(brands, {'FOREIGN'})
 
     def test_find_brands_to_route_host_no_registrar_not_branded(self):
         brands = self._routing_helper._find_brands_to_route('FOREIGN', None)
-        assert_true(brands == ['FOREIGN'])
+        assert_equal(brands, {'FOREIGN'})
 
     def test_find_brands_to_route_registrar_and_host_both_branded_same(self):
         brands = self._routing_helper._find_brands_to_route('EMEA', 'EMEA')
-        assert_true(brands == ['EMEA'])
+        assert_equal(brands, {'EMEA'})
 
     def test_find_brands_to_route_host_registrar_both_branded_different(self):
         brands = self._routing_helper._find_brands_to_route('GODADDY', 'EMEA')
-        assert_true(brands == ['GODADDY', 'EMEA'])
+        assert_equal(brands, {'GODADDY', 'EMEA'})
 
     def test_find_brands_to_route_registrar_branded_hosted_not_branded(self):
         brands = self._routing_helper._find_brands_to_route('FOREIGN', 'EMEA')
-        assert_true(brands == ['FOREIGN', 'EMEA'])
+        assert_equal(brands, {'FOREIGN', 'EMEA'})
 
     def test_find_brands_to_route_foreign(self):
         brands = self._routing_helper._find_brands_to_route('FOREIGN', 'FOREIGN')
-        assert_true(brands == ['FOREIGN'])
+        assert_equal(brands, {'FOREIGN'})
+
+    def test_find_brands_to_route_123reg_hosted(self):
+        brands = self._routing_helper._find_brands_to_route('123REG', 'EMEA')
+        assert_equal(brands, {'123REG', 'EMEA'})
+
+    def test_find_brands_to_route_123reg_registered(self):
+        brands = self._routing_helper._find_brands_to_route('EMEA', '123REG')
+        assert_equal(brands, {'EMEA', '123REG'})
+
+    def test_find_brands_to_route_123reg_registered_and_hosted(self):
+        brands = self._routing_helper._find_brands_to_route('123REG', '123REG')
+        assert_equal(brands, {'123REG'})
+
+    def test_find_brands_to_route_123reg_hosted_foreign_registered(self):
+        brands = self._routing_helper._find_brands_to_route('123REG', 'FOREIGN')
+        assert_equal(brands, {'GODADDY'})
+
+    def test_find_brands_to_route_gd_hosted_foreign_registered(self):
+        brands = self._routing_helper._find_brands_to_route('GODADDY', 'FOREIGN')
+        assert_equal(brands, {'GODADDY'})
+
+    def test_find_brands_to_route_123reg_hosted_gd_registered(self):
+        brands = self._routing_helper._find_brands_to_route('123REG', 'GODADDY')
+        assert_equal(brands, {'GODADDY'})
 
     @patch.object(RoutingHelper, '_route_to_brand')
     def test_route_emea_only(self, _route_to_brand):
@@ -61,8 +85,8 @@ class TestRoutingHelper:
         assert_equal(returned_data, self._routing_helper._db.get_incident('1234'))
 
     @patch.object(RoutingHelper, '_route_to_brand')
-    def test_route_godaddy_foreign(self, _route_to_brand):
-        ticket_data = {'ticketId': '1234', 'data': {'domainQuery': {'host': {'brand': 'GODADDY'},
+    def test_route_godaddy(self, _route_to_brand):
+        ticket_data = {'ticketId': '1235', 'data': {'domainQuery': {'host': {'brand': 'GODADDY'},
                                                                     'registrar': {'brand': 'FOREIGN'}}}}
         returned_data = self._routing_helper.route(ticket_data)
         assert_equal(returned_data, ticket_data)
