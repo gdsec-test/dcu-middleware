65a132894705d094b9ae20a03369c1548b9a1c9a
Nathan Lemay <nlemay@godaddy.com>
2019-01-22 10:21:16 -0800

    Adds support for pip 18.1 and backwards compatibility for <9.0.3 (#80)

--- setup.py
+++ setup.py
@@ -1,25 +1,27 @@
-from pip.req import parse_requirements
 from setuptools import find_packages, setup
 
-# parse_requirements() returns generator of pip.req.InstallRequirement objects
-install_reqs = parse_requirements('requirements.txt', session=False)
-testing_reqs = parse_requirements('test_requirements.txt', session=False)
+with open('requirements.txt') as f:
+    install_reqs = f.read().splitlines()
 
-# reqs is a list of requirement
-# e.g. ['django==1.5.1', 'mezzanine==1.4.6']
-reqs = [str(ir.req) for ir in install_reqs]
-test_reqs = [str(ir.req) for ir in testing_reqs]
+with open('test_requirements.txt') as f:
+    testing_reqs = f.read().splitlines()
 
+with open('README.md') as f:
+    long_description = f.read()
 
 setup(
     name='dcumiddleware',
     version='1.0',
-    description='middleware for processing API requests',
     author='DCU',
     author_email='dcueng@godaddy.com',
+    description='middleware for processing API requests',
+    long_description=long_description,
     url='https://github.secureserver.net/ITSecurity/DCUMiddleware',
-    # packages=packages,
-    packages=find_packages(exclude=['tests', 'tests.*']),
-    install_requires=reqs,
-    tests_require=test_reqs,
-    test_suite="nose.collector")
+    packages=find_packages(exclude=['tests']),
+    install_requires=install_reqs,
+    tests_require=testing_reqs,
+    test_suite='nose.collector',
+    classifiers=[
+        'Programming Language :: Python :: 2.7'
+    ]
+)
